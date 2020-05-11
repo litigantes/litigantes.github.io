c56087ee97791b6b78a7c2f4e48b900b96e2bd1a
litigantes <65190222+litigantes@users.noreply.github.com>
2020-05-11 16:58:08 -0500

    add array.php

---
+++ array.php
@@ -0,0 +1,16 @@
+﻿<?php
+//$array = array('estado'=> 1, '' );
+
+$array = array(
+    'estado'=>'1',
+    'gen_noticiasjudiciales' => array(        
+		'NOJ_IdNoticia'=> '5',
+		'NOJ_Titular'=> 'Asuntos Legales',
+		'NOJ_Texto'=> 'Edictos',
+		'NOJ_Link'=> 'https://www.asuntoslegales.com.co/edictos',
+		'NOJ_Estado'=> '1'        
+	)
+);
+
+echo json_encode($array);
+?>
