ff6e55239dff16ec877ebb4977ea8d10534334f6
litigantes <65190222+litigantes@users.noreply.github.com>
2020-05-12 10:45:47 -0500

    upd file

--- upload.php
+++ upload.php
@@ -1,7 +1,8 @@
 <?php
-print_r($_POST);
-print_r($_FILES);
 echo "*******************<br>";
 echo json_encode($gen_noticiasjudiciales);
 echo "dato1.....".$gen_noticiasjudiciales['gen_noticiasjudiciales']['NOJ_Titular'];
+print_r($_POST);
+print_r($_FILES);
+
 ?>
